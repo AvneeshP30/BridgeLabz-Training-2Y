17e137d039282bd8d74d998d94895d9f0b1d27e3
Avneesh Pandey <avneeshpandey.3005@gmail.com>
2025-10-25 21:13:14 +0530

    Delete Lambda/src directory

--- Lambda/src/PatientID.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-public class PatientID {
-    public static void main(String[] args) {
-        List<String> patientIDs = Arrays.asList("P101", "P102", "P103", "P104");
-
-        // Using method reference instead of lambda
-        patientIDs.forEach(System.out::println);
-    }
-}
