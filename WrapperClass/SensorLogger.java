5db8fb1bf4d9d0045e087c6c7d2b9c6f0ee1dbc1
Avneesh Pandey <avneeshpandey.3005@gmail.com>
2025-11-06 14:40:39 +0530

    Delete WrapperClass directory

--- WrapperClass/SensorLogger.java
+++
@@ -1,9 +0,0 @@
-
-public class SensorLogger {
-    static void log(double t){System.out.println(t);}
-    static void log(Double t){System.out.println(t);}
-    public static void main(String[] args){
-        double a=36.5;Double b=37.8;
-        log(a);log(b);
-    }
-}
